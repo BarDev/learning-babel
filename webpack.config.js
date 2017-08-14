741b597e831ea1c6f137411043291da37fa14d24
BarDev <github@BarDev.com>
2017-08-13 19:00:01 -0600

    now multiple entries in webpack, and multiple destinations.  Alos, HtmlWebpackPlugin will add script to top of HTML

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+let webpack = require("webpack");
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let path = require('path');
+
+module.exports = {
+    resolve: {
+        extensions: ['.js']
+    },
+    entry: {
+        polyfill: "babel-polyfill", //node_modules/babel-polyfill/polyfill.js
+        bundle: path.resolve(__dirname, 'src/index.js') // Defining path seems necessary for this to work consistently on Windows machines.
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                loader: "babel-loader",
+                query: {
+                    presets: []
+                }
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({     // Create HTML file that includes references to bundled CSS and JS.
+            template: 'src/index.html',
+            filename: "index.html",
+            inject: "head"
+        }),
+    ],
+};
