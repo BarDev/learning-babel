f3823497e0a40123d6fffb8b3cddcb3adb0367f1
BarDev <github@BarDev.com>
2017-08-27 19:47:34 -0600

    add script to use es6/es2015 feature startsWith

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log("Starting in index.js")
+
+/*
+    ES6/ES2015 - String.prototype.startsWith 
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
+    Compatiblity:
+        - Chorme:             yes
+        - IE11:               no
+        - Babel-Polyfill:     yes
+        - Babel-runtime:      no        
+        - Babel-Transpile:    no
+******************************************** */
+let letters = "abcdefgh";
+let doesStartWithA = letters.startsWith("a")
+console.log(doesStartWithA)
