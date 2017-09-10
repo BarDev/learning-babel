e3e7aea18d398c92bcec157a1cc42bf5e978c1ed
BarDev <github@BarDev.com>
2017-09-10 15:54:08 -0600

    Add polyfile manually to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 console.log("Starting in index.js")
 
+// Polyfill code from - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith#Polyfill
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position){
+      return this.substr(position || 0, searchString.length) === searchString;
+  };
+}
+
 /*
     ES6/ES2015 - String.prototype.startsWith 
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
